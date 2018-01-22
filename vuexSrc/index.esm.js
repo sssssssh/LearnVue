18e555f7a16db0d65d2ac745dcd4f7f7fba6fd46
申邵辉 <shenshaohui@ihuayue.cn>
2018-01-22 13:37:28 +0800

    init vuex code

--- vuexSrc/index.esm.js
+++ vuexSrc/index.esm.js
@@ -2,22 +2,22 @@ import { Store, install } from './store'
 import { mapState, mapMutations, mapGetters, mapActions, createNamespacedHelpers } from './helpers'
 
 export default {
-  Store,
-  install,
-  version: '__VERSION__',
-  mapState,
-  mapMutations,
-  mapGetters,
-  mapActions,
-  createNamespacedHelpers
+    Store,
+    install,
+    version: '__VERSION__',
+    mapState,
+    mapMutations,
+    mapGetters,
+    mapActions,
+    createNamespacedHelpers
 }
 
 export {
-  Store,
-  install,
-  mapState,
-  mapMutations,
-  mapGetters,
-  mapActions,
-  createNamespacedHelpers
+    Store,
+    install,
+    mapState,
+    mapMutations,
+    mapGetters,
+    mapActions,
+    createNamespacedHelpers
 }
--- vuexSrc/plugins/devtool.js
+++ vuexSrc/plugins/devtool.js
@@ -1,19 +1,19 @@
 const devtoolHook =
-  typeof window !== 'undefined' &&
-  window.__VUE_DEVTOOLS_GLOBAL_HOOK__
+    typeof window !== 'undefined' &&
+    window.__VUE_DEVTOOLS_GLOBAL_HOOK__
 
-export default function devtoolPlugin (store) {
-  if (!devtoolHook) return
+export default function devtoolPlugin(store) {
+    if (!devtoolHook) return
 
-  store._devtoolHook = devtoolHook
+    store._devtoolHook = devtoolHook
 
-  devtoolHook.emit('vuex:init', store)
+    devtoolHook.emit('vuex:init', store)
 
-  devtoolHook.on('vuex:travel-to-state', targetState => {
-    store.replaceState(targetState)
-  })
+    devtoolHook.on('vuex:travel-to-state', targetState => {
+        store.replaceState(targetState)
+    })
 
-  store.subscribe((mutation, state) => {
-    devtoolHook.emit('vuex:mutation', mutation, state)
-  })
+    store.subscribe((mutation, state) => {
+        devtoolHook.emit('vuex:mutation', mutation, state)
+    })
 }
